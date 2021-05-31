642a96c004b8bb5225947c29feaf7d0718029561
RodrigoInawashiro <83737949+RodrigoInawashiro@users.noreply.github.com>
2021-05-30 21:00:06 -0300

    Update index.js

--- semana1/strings-arrays/index.js
+++ semana1/strings-arrays/index.js
@@ -1,56 +1,144 @@
-//Exercícios de interpretação de código
+// Exemplos
 
-//Exercicio 1
+// Exercício 0A
+function soma() {
+  // escreva seu código aqui
+  const num1 = prompt('Digite o primeiro número')
+  const num2 = prompt('Digite o segundo número')
 
-//a) Como não foi declarada seria: undefined
+  console.log(Number(num1) + Number(num2))
+}
 
-//b) Null
+// Exercício 0B
+function imprimeMensagem() {
+  // escreva seu código aqui
+  const mensagem = prompt('Digite sua mensagem')
 
-//c) 11
+  console.log(mensagem)
+}
 
-//d) 3
+// ---------------------------------------------------
+// Exercícios
 
-//e) como declarou a array como 19, console.log imprime 19
+// Exercício 1
+function calculaAreaRetangulo() {
+  
+  const altura = Number(prompt("Digite a altura:"))
+  const largura = Number(prompt("Digite a largura:"))
+  const areaRetangulo = altura*largura
+  console.log(areaRetangulo)
 
-//f)9
+}
 
-// Exercicio 2
+// Exercício 2
+function imprimeIdade() {
+  // escreva seu código aqui
+  let anoAtual = Number(prompt("Digite o ano atual:"))
+  let nascimento = Number(prompt("Digite o seu ano de nascimento:"))
+  const idade = anoAtual-nascimento
+  console.log(idade)
+}
 
-// O valor impresso será 27
+// Exercício 3
+function calculaIMC() {
+  // escreva seu código aqui
+  let peso = Number(prompt("Digite o seu peso:"))
+  let altura = Number(prompt("Digite a sua altura:"))
+  const imc = peso/(altura*altura)
+  console.log(imc)
+
+}
+
+// Exercício 4
+function imprimeInformacoesUsuario() {
+  // escreva seu código aqui
+  const nome= prompt("Digite o seu nome:")
+  const idade = prompt("Digite a sua idade:")
+  const email = prompt("Digite o seu email:")
+  console.log("Meu nome é " + nome + ", tenho " + idade +" anos, e o meu email é " + email+".")
+}
+
+
+// Exercício 5
+function imprimeTresCoresFavoritas() {
+  // escreva seu código aqui
+  let coresFavoritas1 = prompt("Digite sua primeira cor favorita:")
+  let coresFavoritas2 = prompt("Digite sua segunda cor favorita:")
+  let coresFavoritas3 = prompt("Digite sua terceira cor favorita:")  
+  const arrayCores = [coresFavoritas1,coresFavoritas2,coresFavoritas3]
+
+  console.log(arrayCores)
+  
+
+
+}
+
+// Exercício 6
+function retornaStringEmMaiuscula() {
+  // escreva seu código aqui
+  const comida = prompt("Digite sua comida preferida:")
+  console.log(comida.toUpperCase())
+}
+
+// Exercício 7
+function calculaIngressosEspetaculo() {
+  // escreva seu código aqui
+  let custoEspetaculo = Number(prompt("Diga qual o custo do espetaculo:"))
+  let valorIngresso = Number(prompt("Diga qual valor de cada ingresso:"))
+  console.log(custoEspetaculo/valorIngresso)
+
+}
+
+// Exercício 8
+function checaStringsMesmoTamanho() {
+  // escreva seu código aqui
+  let string1 = prompt("Diga sua cor preferida:")
+  let string2 = prompt("Diga outra cor :")
+  console.log(string1.length === string2.length)
+}
+
+// Exercício 9
+function checaIgualdadeDesconsiderandoCase() {
+  // escreva seu código aqui
+  let comida1 = prompt("Diga o nome de uma comida:")
+  let comida2 = prompt("Diga uma sobremesa:")
+  console.log(comida1.toLowerCase() === comida2.toLowerCase())
 
 
 
-//Exercícios de escrita de código
+}
 
-//Exercicio 1
+// Exercício 10
+function checaRenovacaoRG() {
+  // escreva seu código aqui
+  const anoAtual = Number(prompt("Digite o ano atual:"))
+  const anoDeNascimento = Number(prompt("Digite seu ano de nascimento:"))
+  const anoEmissao = Number(prompt("Digite ano em que sua carteira de identidade foi emitida:"))
+  const idade = anoAtual - anoDeNascimento
+  const tempoDeemissao = anoAtual - anoEmissao
 
-/*let nome = prompt("Digite seu nome:")
-let email = prompt("Digite seu email:")
- 
-console.log("O e-mail ${email} foi cadastrado com sucesso. Seja bem-vinda(o), ${nome}!")*/
+  
+  console.log((idade <= 20 && tempoDeemissao >= 5) || (idade > 20 && idade <= 50 && tempoDeemissao >= 10) || (idade > 50 && tempoDeemissao >= 15))
 
 
 
+}
 
-//Exercicio 2
+// Exercício 11
+function checaAnoBissexto() {
+  // escreva seu código aqui
+  const ano = Number(prompt("Digite um ano:"))
+  console.log((ano % 400 === 0) || (ano % 4 === 0) && (ano % 100 != 0))
 
-/*const comidas = ["bolo", "torta", "pizza", "sanduiche", "churrasco"]
-console.log("comidas", comidas)
+}
 
-console.log("1:",comidas[0])
-console.log("2:",comidas[1])
-console.log("3:",comidas[2])
-console.log("4:",comidas[3])
-console.log("5:",comidas[4])*/
+// Exercício 12
+function checaValidadeInscricaoLabenu() {
+  // escreva seu código aqui
 
-//Exercicio 3
+  let labenuIdade = (prompt("Você tem mais de 18 anos?"))
+  let labenuescolaridade = (prompt("Você possui ensino médio completo?"))
+  let labenuDisponibilidade = (prompt("Você possui disponibilidade exclusiva durante os horários do curso?"))
 
-/*let listaDeTarefas = []
-listaDeTarefas1 = prompt("Digite sua atividade no dia")
-listaDeTarefas2 = prompt("Digite sua atividade no dia")
-listaDeTarefas3 = prompt("Digite sua atividade no dia")
-listaDeTarefas.push(listaDeTarefas1,listaDeTarefas2,listaDeTarefas3)
-console.log(listaDeTarefas)
-const indice = prompt("Digite o indice de uma tarefa 0, 1 ou 2")
-listaDeTarefas.splice(indice, 1)
-console.log(listaDeTarefas)*/
+  console.log(labenuIdade == "sim" && labenuescolaridade == "sim" && labenuDisponibilidade == "sim")
+
