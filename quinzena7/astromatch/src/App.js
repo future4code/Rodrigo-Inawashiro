fb415d1841475bcdbcc198bd0ffe76a38f336f21
Rodrigo Inawashiro <rodrigoinaw@hotmail.com>
2021-09-02 09:31:36 -0300

    primeiro-commit

---
+++ quinzena7/astromatch/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import styled from "styled-components";
+import axios  from 'axios';
+import { useState, useEffect} from 'react';
+
+const App = () => {
+  const [perfil, setPerfil] = useState[0];
+
+  
+}
+
+export default App;
+
+
